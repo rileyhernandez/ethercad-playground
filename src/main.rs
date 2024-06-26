e1657278cc3ede07eeb0f3304e3bb4e144981ab8
Riley <rileykhernandez@gmail.com>
2024-06-26 16:49:00 -0700

    Implemented actor for sealer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 mod sealer;
-use crate::sealer::Sealer;
-use std::time::Duration;
+use crate::sealer::{Sealer, SealerCommand};
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
+    env_logger::init();
+
     let sealer = Sealer::new(0x02, 0x01, 0x08, 0x04);
 
-    sealer.open_door(Duration::from_secs_f64(5.3f64)).await?;
-    // sealer.close_door(Duration::from_secs_f64(5.3f64)).await?;
-    // sealer.apply_heater(Duration::from_secs_f64(7f64)).await?;
-    // sealer.remove_heater(Duration::from_secs_f64(7f64)).await?;
+    let (tx, rx) = mpsc::channel(10);
+    let sealer_handler = tokio::spawn(async move { sealer.actor(rx).await });
 
+    tx.send(SealerCommand::CloseDoor(5.3))
+        .await
+        .expect("Open door failed");
+    tx.send(SealerCommand::ApplyHeater(7f64))
+        .await
+        .expect("Apply heater failed");
+    tx.send(SealerCommand::RemoveHeater(7f64))
+        .await
+        .expect("Remove heater failed");
+    tx.send(SealerCommand::OpenDoor(5.3))
+        .await
+        .expect("Close door failed");
+    let _ = { tx };
+    sealer_handler.await.expect("Sealer Actor failed");
     Ok(())
 }
--- src/sealer.rs
+++ src/sealer.rs
@@ -1,9 +1,8 @@
-use env_logger::Env;
 use ethercrab::{
-    error::Error,
     std::{ethercat_now, tx_rx_task},
     Client, ClientConfig, PduStorage, Timeouts,
 };
+use log::info;
 use std::{
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -11,12 +10,13 @@ use std::{
     },
     time::Duration,
 };
-use tokio::time;
-use tokio::time::{timeout, MissedTickBehavior};
+use tokio::sync::mpsc::error::TryRecvError;
+use tokio::sync::mpsc::Receiver;
+use tokio::time::MissedTickBehavior;
 
 const MAX_SLAVES: usize = 16;
-const MAX_PDU_DATA: usize = PduStorage::element_size(1100);
-const MAX_FRAMES: usize = 16;
+pub(crate) const MAX_PDU_DATA: usize = PduStorage::element_size(1100);
+pub(crate) const MAX_FRAMES: usize = 16;
 const PDI_LEN: usize = 64;
 const INTERFACE: &str = "enp1s0f0";
 
@@ -43,37 +43,8 @@ impl Sealer {
         }
     }
 
-    pub async fn open_door(&self, duration: Duration) -> Result<(), ()> {
-        Self::loop_with_timer(self.open_door_byte, duration).await
-    }
-
-    pub async fn close_door(&self, duration: Duration) -> Result<(), ()> {
-        Self::loop_with_timer(self.close_door_byte, duration).await
-    }
-
-    pub async fn apply_heater(&self, duration: Duration) -> Result<(), ()> {
-        Self::loop_with_timer(self.apply_heater_byte, duration).await
-    }
-
-    pub async fn remove_heater(&self, duration: Duration) -> Result<(), ()> {
-        Self::loop_with_timer(self.remove_heater_byte, duration).await
-    }
-
-    async fn loop_with_timer(command_byte: u8, duration: Duration) -> Result<(), ()> {
-        let loop_command = Self::loop_command(command_byte);
-        match timeout(duration, loop_command).await {
-            Ok(_) => {
-                eprintln!("Ethercrab loop ended abruptly");
-                Err(())
-            }
-            Err(_) => Ok(()),
-        }
-    }
-
-    async fn loop_command(command_byte: u8) {
-        // helper to loop given byte command from above fn
-        log::info!("Starting ethercrab...");
-
+    pub async fn actor(&self, mut sealer_rx: Receiver<SealerCommand>) {
+        info!("Starting ethercrab...");
         let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("Can only split once");
         let client = Arc::new(Client::new(
             pdu_loop,
@@ -84,16 +55,16 @@ impl Sealer {
             },
             ClientConfig::default(),
         ));
-        tokio::spawn(tx_rx_task(&INTERFACE, tx, rx).expect("Spawn TX/RX task"));
+        tokio::spawn(tx_rx_task(INTERFACE, tx, rx).expect("Spawn TX/RX task"));
         let group = client
             .init_single_group::<MAX_SLAVES, PDI_LEN>(ethercat_now)
             .await
             .expect("Init");
-        log::info!("Discovered {:?} slaves", group.len());
+        info!("Discovered {:?} slaves", group.len());
         let mut group = group.into_op(&client).await.expect("Pre-Op -> Op");
         for slave in group.iter(&client) {
             let (i, o) = slave.io_raw();
-            log::info!(
+            info!(
                 "Slave {:#06x} {} inputs: {} bytes, outputs: {} bytes",
                 slave.configured_address(),
                 slave.name(),
@@ -106,22 +77,52 @@ impl Sealer {
         let shutdown = Arc::new(AtomicBool::new(false));
         signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&shutdown))
             .expect("Register hook");
+
         loop {
-            if shutdown.load(Ordering::Relaxed) {
-                log::info!("Shutting down...");
-                break;
-            }
-            group.tx_rx(&client).await.expect("TX/RX");
-            let mut slave = group.slave(&client, 1).unwrap();
-            let (_, o) = slave.io_raw_mut();
-            o[0] = command_byte;
-            tick_interval.tick().await;
+            match sealer_rx.try_recv() {
+                Ok(cmd) => {
+                    let (cmd_byte, duration) = match cmd {
+                        SealerCommand::OpenDoor(duration) => (self.open_door_byte, duration),
+                        SealerCommand::CloseDoor(duration) => (self.close_door_byte, duration),
+                        SealerCommand::ApplyHeater(duration) => (self.apply_heater_byte, duration),
+                        SealerCommand::RemoveHeater(duration) => {
+                            (self.remove_heater_byte, duration)
+                        }
+                    };
+                    let init_time = tokio::time::Instant::now();
+                    let delay = Duration::from_secs_f64(duration);
+                    while tokio::time::Instant::now() - init_time < delay {
+                        group.tx_rx(&client).await.expect("TX/RX");
+                        let mut slave = group.slave(&client, 1).unwrap();
+                        let (_, o) = slave.io_raw_mut();
+
+                        if shutdown.load(Ordering::Relaxed) {
+                            info!("Shutting down...");
+                            break;
+                        }
+                        info!("thinks it's doing something ig?");
+                        o[0] = cmd_byte;
+                        tick_interval.tick().await;
+                    }
+                }
+                Err(e) => match e {
+                    TryRecvError::Empty => continue,
+                    TryRecvError::Disconnected => break,
+                },
+            };
         }
         let group = group.into_safe_op(&client).await.expect("Op -> Safe-Op");
-        log::info!("Op -> Safe-Op");
+        info!("Op -> Safe-Op");
         let group = group.into_pre_op(&client).await.expect("Safe-Op -> Pre-Op");
-        log::info!("Safe-Op -> Pre-Op");
+        info!("Safe-Op -> Pre-Op");
         let _group = group.into_init(&client).await.expect("Pre-Op -> Init");
-        log::info!("Pre-Op -> Init... Shutdown Complete!");
+        info!("Pre-Op -> Init... Shutdown Complete!");
     }
+}
+
+pub enum SealerCommand {
+    OpenDoor(f64),
+    CloseDoor(f64),
+    ApplyHeater(f64),
+    RemoveHeater(f64),
 }
